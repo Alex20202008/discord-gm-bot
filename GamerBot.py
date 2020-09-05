f953af37d963bdb988b81a7ddb9129ecd148df07
Alex <66314039+Alex20202008@users.noreply.github.com>
2020-09-05 23:22:04 +0300

    Update GamerBot.py

--- GamerBot.py
+++ GamerBot.py
@@ -12,11 +12,16 @@ bot = commands.Bot(command_prefix = prefix)
 bot.remove_command("help")
 
 
-# when online
+# events
 @bot.event
 async def on_ready():
 	activity = discord.Activity(name='!!', type=discord.ActivityType.listening)
 	await bot.change_presence(activity=activity)
+
+@bot.event
+async def on_member_join(user):
+	channel_join = bot.get_channel(719185068349718598)
+	await channel_join.send(f"{user.mention} зашел на сервер:)")
 
 
 # commands
