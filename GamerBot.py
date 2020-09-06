fadc6025dc83f7a01dc4f9b878b1732c2fe50d2d
Alex <66314039+Alex20202008@users.noreply.github.com>
2020-09-06 11:19:21 +0300

    Update GamerBot.py

--- GamerBot.py
+++ GamerBot.py
@@ -32,9 +32,8 @@ async def on_member_remove(user):
 
 @bot.command()
 async def members(ctx):
-	# member_count = len(ctx.guild.members)
-	true_member_count = len([for m in ctx.guild.members if not m.bot])
-	await bot.send_message(f"На сервере {true_member_count} геймеров:)")
+    member_count = len(ctx.guild.members) # includes bots
+    await ctx.send(member_count)
 
 # START!!
 bot.run(TOKEN)
